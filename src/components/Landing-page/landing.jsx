ba2604335868fae19921d13bd7128923be8e4922
abdulGitHb <abdulsamad6876@gmail.com>
2023-01-27 18:12:18 +0530

    added bubbles in the backGround

--- src/components/Landing-page/landing.jsx
+++ src/components/Landing-page/landing.jsx
@@ -15,34 +15,46 @@ export default function Landing() {
 
 
     return (
+        <>
+
         <section className='hero_container' id="hero-home">
-            <div className='bubble'>
-                <span></span>
-                <span></span>
-                <span></span>
-            </div>
+            
             <div className="hero_home_video_hld">
                 <div className="hero_home_contact_btn">Contact Us</div>
                 <video src="https://gravityteam.co/wp-content/uploads/2022/07/hero-animation.mp4" className='hero_home_video' muted={true} loop={true} autoPlay={true}></video>
             </div>
             <div className="hero_home_content">
-                <span className='typewriter' style={{ color: '#fff' }}>Handling</span>
-                <span className='typewriter' style={{ color: '#0090ee', fontWeight: 'bold' }}>
-                    <Typewriter
-                        words={['Web Development', 'App Development']}
-                        loop={Infinity}
-                        cursor
-                        cursorStyle='|'
-                        typeSpeed={70}
-                        deleteSpeed={50}
-                        delaySpeed={1000}
-                    />
-                </span>
-                <div className="hero_home_content_desc">
-                    <span>We help you turn ideas into reality. Let’s build
-                        something amazing together</span >
+                <div className='bubble'>
+                        <span className='inbubble'></span>
+                        <span className='inbubble'></span>
+                        <span className='inbubble'></span>
+                        <span className='inbubble'></span>
+                        <span className='inbubble'></span>
+                        <span className='inbubble'></span>
+                        <span className='inbubble'></span>
+                        <span className='inbubble'></span>
+                        <span className='inbubble'></span>
+                        <span className='inbubble'></span>
+                        <span className='inbubble'></span>
                 </div>
-            </div>
+                    <span className='typewriter' style={{ color: '#fff' }}>Handling</span>
+                    <span className='typewriter' style={{ color: '#0090ee', fontWeight: 'bold' }}>
+                        <Typewriter
+                            words={['Web Development', 'App Development']}
+                            loop={Infinity}
+                            cursor
+                            cursorStyle='|'
+                            typeSpeed={70}
+                            deleteSpeed={50}
+                            delaySpeed={1000}
+                        />
+                    </span>
+                    <div className="hero_home_content_desc">
+                        <span>We help you turn ideas into reality. Let’s build
+                            something amazing together</span >
+                    </div>
+                </div>
+ 
             <div className="hero_home_social">
                 <span className='hero_home_social_icon'><ImFacebook size={30} /></span>
                 <span className='hero_home_social_icon'><ImLinkedin2 size={30} /></span>
@@ -50,5 +62,8 @@ export default function Landing() {
                 <span className='hero_home_social_icon'><FaPinterestP size={30} /></span>
             </div>
         </section>
+
+            
+        </>
     )
 }
