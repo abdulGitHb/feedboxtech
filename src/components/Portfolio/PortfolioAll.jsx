d2e309f254087f24b7bb6725d0c68fc9f1cea586
abdulGitHb <abdulsamad6876@gmail.com>
2023-02-17 19:03:10 +0530

    all error fixed

--- src/components/Portfolio/PortfolioAll.jsx
+++ src/components/Portfolio/PortfolioAll.jsx
@@ -1,71 +1,17 @@
-// // // import React, { useEffect } from 'react'
-// // // import './Portfolio.css'
-// // // import Img from '../../image/holiday.jpg'
-// // import Centaurus from '../../image/centaurus.png'
-// // import FuteServices from '../../image/futeServices.png'
-// // import SketchMyPlot from '../../image/sketchMyPlot.png'
-// // import TintedTales from '../../image/tintedTales.png'
-// // import IndependenceRock from '../../image/independenceRock.png'
-// // import Qodeleaf from '../../image/qodeleaf.png'
-// // import Tenet from '../../image/tenet.png'
-// // import Runo from '../../image/runo.png'
-// // import Nh7 from '../../image/nh7.png'
-// // import OnFinance from '../../image/onFinance.png'
-// // import MakeBelieve from '../../image/makeBelieve.png'
-// // import Gallant_jwellery from '../../image/gallent_jwellery.png'
-// // import Aryan from '../../image/aryan.png'
-// // // import Card from '../PortfolioCard/PortfolioCard'
-// // // import AOS from 'aos';
-// // // import 'aos/dist/aos.css';
-// // // import Footer from '../Footer/Footer'
-
-
-
-// import React from "react";
-// import Swiper from "./Swiperr";
-// export default function PortfolioAll() {
-// return (
-// <>
-// <div style={{background:"black",height:"800px",top:0,position:"relative",overflowX:"hidden"}}>
-// <Swiper/>
-
-// </div>
-
-// </>
-//     )
-// }
 import React, { useEffect } from 'react'
 import './Portfolio.css'
-// import Img from '../../image/holiday.jpg'
-import Centaurus from '../../image/centaurus.png'
-import FuteServices from '../../image/futeServices.png'
-import SketchMyPlot from '../../image/sketchMyPlot.png'
-import TintedTales from '../../image/tintedTales.png'
-import IndependenceRock from '../../image/independenceRock.png'
-import Qodeleaf from '../../image/qodeleaf.png'
-import Tenet from '../../image/tenet.png'
-import Runo from '../../image/runo.png'
-import Nh7 from '../../image/nh7.png'
-import OnFinance from '../../image/onFinance.png'
-import MakeBelieve from '../../image/makeBelieve.png'
-import Gallant_jwellery from '../../image/gallent_jwellery.png'
-import Aryan from '../../image/aryan.png'
-import Card from '../PortfolioCard/PortfolioCard'
-import AOS from 'aos';
+
 import 'aos/dist/aos.css';
-import Footer from '../Footer/Footer'
 import Whatsapp from '../Whatsapp/Whatsapp'
 import Panorma from './Panorma'
 
 export default function PortfolioAll() {
 return (
-<>
+        <>
 
-<Panorma/>
+            <Panorma/>
 
-            {/* </div> */}
             <Whatsapp />
-            <Footer />
         </>
 
     )
