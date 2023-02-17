c2f39a46d2d3c55b23b4602efe5c72d2f40f9857
Aditya Pandey <adityapandey272002@gmail.com>
2023-02-17 17:52:46 +0530

    about us page completely done

--- src/components/AboutUsFullPage/aboutUsPage1.jsx
+++ src/components/AboutUsFullPage/aboutUsPage1.jsx
@@ -29,20 +29,26 @@ import "./aboutUsPage1.css";
 import {Parallax} from "react-parallax";
 import { ParallaxProvider ,ParallaxBanner } from 'react-scroll-parallax';
 import ParallaxDemo from './parallaxDemo';
+import { Link } from 'react-router-dom';
 
 function AboutUsPage1() {
     return (
       <>
-      <NavBarResp/>
+      {/* <NavBarResp/> */}
 
       <ParallaxProvider>
+        <div className='aditya-about-desktop'>
         <Parallax   className="aditya-parall">
         <div className="aditya-about">
           <Parallax   className="aditya-parallax1">
           <div className="aditya-text-content0">About Us</div>
           </Parallax>
 
-          <Parallax  bgImage={img121} className="aditya-parallax" strength={-300} style={{backgroundPositon:"center"}}>
+          <Parallax  
+          bgImage={img121} 
+          className="aditya-parallax" strength={-300} 
+        
+          >
           <div className="aditya-text-content">
           <p className='aditya-p-text'>
              <span className="aditya-b">F</span>eedbox was awarded the MP Young Achievers Award by the CM of Madhya Pradesh Shri Shivraj Singh Chouhan. We were among the top 10 businesses to receive a banner shoutout at the CM house in Bhopal.
@@ -92,8 +98,74 @@ function AboutUsPage1() {
         </div>
         
         </Parallax>
-     
+        </div>
+
+        {/* for mobile */}
+        <div className='aditya-about-mobile'>
+           <Parallax   className="aditya-parall">
+        <div className="aditya-about">
+          <Parallax   className="aditya-parallax1">
+          <div className="aditya-text-content0">About Us</div>
+          </Parallax>
+
+          <Parallax  
+          bgImage={img1} 
+          className="aditya-parallax" strength={-300} 
+        
+          >
+          <div className="aditya-text-content">
+          <p className='aditya-p-text'>
+             <span className="aditya-b">F</span>eedbox was awarded the MP Young Achievers Award by the CM of Madhya Pradesh Shri Shivraj Singh Chouhan. We were among the top 10 businesses to receive a banner shoutout at the CM house in Bhopal.
+              </p>
+          </div>
+         
+          
+          </Parallax>
+          <Parallax   bgImage={img2} className="aditya-parallax" >
+          <div className="aditya-text-content aditya-text-content1">
+          <p className='aditya-p-text'>
+          <span className="aditya-b">F</span>eedbox became the first startup in Indore to receive this accolade. 3 years in this industry, our consistency and hard work were met by the prestigious certificate which is still a dream for many budding businesses.
+              </p>
+          </div>
+          </Parallax>
+          <Parallax bgImage={img3} className="aditya-parallax" strength={-300}>
+          <div className="aditya-text-content">
+          <p className='aditya-p-text'>
+          <span className="aditya-b">F</span>eedbox was founded by 2 engineers,Yash & Ayushi, who had a shared vision of starting a digital marketing company. The company's focus on revenue growth has helped many startups to scale their businesses and achieve their financial goals in the past 3 years.
+              </p>
+          </div>
+          </Parallax>
+          <div className='aditya-about-story'>
+               {/* about us story */}
+                 <div className="aditya-story-head">Our Story</div>
+                 <div className='aditya-story-text'> 
+                     <div>
+                     Every success story has a humble beginning. Ours was driven
+                     by an aim to disrupt the status quo in the future of marketing! 
+                     Our founder's journey goes back to 20XX, while he was still in 
+                     college, promoting and marketing events and concerts in Indore.
+                     He was training his juniors and working with them all the same 
+                     to attain experience. <br/>
+                     </div>
+                     <div>
+                     In his final year, Yash had IIM calls as well as placement offers
+                     in several top notch companies across the country but he'd rather 
+                     hustle to make his dream work than punch clock for someone else's.
+                     The paucity of funds resulted him in taking up a temporary job in 
+                     Indore to provide for his startup. Our now COO joined him around 
+                     that time as an intern.
+                     </div>
+                 </div>
+                 {/* about us story button */}
+                 <Link to='/ourstory'><button className='story-btn'  type="button">Read Full Story</button></Link>
+               </div>
+        </div>
+        
+        </Parallax>
+
+        </div>
         </ParallaxProvider>
+        
         {/* <ParallaxDemo/> */}
         <Footer/>
         </>
